1ae8c51fa5dff81ecb4634409f7607eed9c206dc
Mickey <micksheridan.24@gmail.com>
2019-05-06 11:55:02 -0400

    rearrange builds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  patch "builds/rearrange/:id", to: "builds#rearrange", as: "rearrange_build"
+
+  resources :units, only: [:show, :index]
+  resources :builds, only: [:show, :index, :create, :update]
+  resources :build_units, only: [:index, :show, :create, :update]
 end
